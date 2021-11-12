7bb8a6f32d171a485be833efcb6c7ea3c124311e
Nguyễn Trung Nhẫn <trungnhan21.12@gmail.com>
2021-11-12 19:14:00 +0700

    Open Source Code 2016

---
+++ Control PC Mini Client/home.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class home
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(home))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ID = New System.Windows.Forms.TextBox()
+        Me.Passwords = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.hdt = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(21, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(51, 16)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "ID PC:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(21, 49)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(104, 16)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Password PC:"
+        '
+        'ID
+        '
+        Me.ID.Enabled = False
+        Me.ID.Location = New System.Drawing.Point(78, 20)
+        Me.ID.Name = "ID"
+        Me.ID.Size = New System.Drawing.Size(308, 20)
+        Me.ID.TabIndex = 2
+        Me.ID.Text = "3598127"
+        '
+        'Passwords
+        '
+        Me.Passwords.Enabled = False
+        Me.Passwords.Location = New System.Drawing.Point(130, 46)
+        Me.Passwords.Name = "Passwords"
+        Me.Passwords.Size = New System.Drawing.Size(256, 20)
+        Me.Passwords.TabIndex = 3
+        Me.Passwords.Text = "nhanpro"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 2000
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        Me.Timer2.Interval = 1500
+        '
+        'hdt
+        '
+        Me.hdt.Enabled = True
+        Me.hdt.Interval = 1000
+        '
+        'home
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(402, 82)
+        Me.Controls.Add(Me.Passwords)
+        Me.Controls.Add(Me.ID)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "home"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Control PC Mini Client"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ID As TextBox
+    Friend WithEvents Passwords As TextBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents hdt As Timer
+End Class
---
+++ Control PC Mini Client/home.vb
@@ -0,0 +1,121 @@
+﻿Imports Microsoft.Win32
+
+Public Class home
+    Dim sv As String
+
+    Private Sub home_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        RunAtStartup(Application.ProductName, Application.ExecutablePath)
+        '   Me.Visible = False
+
+        ' RemoveValue(Application.ProductName)
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Try
+            If sv = "yes" Then
+                Dim gets As String = New System.Net.WebClient().DownloadString("http://cpm.luutru360.com/db.txt")
+                If gets = "msg" Then
+                    Dim rest As String = New System.Net.WebClient().DownloadString("http://cpm.luutru360.com/kills.php")
+
+                    chao()
+                ElseIf gets = "offpcmsg" Then
+                    Dim rest2 As String = New System.Net.WebClient().DownloadString("http://cpm.luutru360.com/kills.php")
+
+                    offpcmsg()
+                ElseIf gets = "outpc" Then
+                    Dim rest2 As String = New System.Net.WebClient().DownloadString("http://cpm.luutru360.com/kills.php")
+
+                    outpc()
+
+                ElseIf gets = "rppc" Then
+                    Dim rest2 As String = New System.Net.WebClient().DownloadString("http://cpm.luutru360.com/kills.php")
+
+                    repc()
+
+                ElseIf gets = "logpc" Then
+                    Dim rest2 As String = New System.Net.WebClient().DownloadString("http://cpm.luutru360.com/kills.php")
+
+                    logoff()
+                End If
+            Else
+
+            End If
+        Catch
+
+        End Try
+    End Sub
+
+
+    Sub chao()
+
+        MsgBox("Xin chào!", vbInformation, "Messger")
+
+    End Sub
+
+    Sub offpcmsg()
+
+        MsgBox("Có người đang nhắc nhỡ bạn nghỉ ngơi và tắt máy tính. Hãy nghỉ ngơi trước khi có chuyện không vui sắp xảy ra!", vbInformation, "Messger")
+
+    End Sub
+    Sub outpc()
+        System.Diagnostics.Process.Start("shutdown", "-s -t 00")
+    End Sub
+    Sub repc()
+        System.Diagnostics.Process.Start("shutdown", "-r -t 00")
+    End Sub
+    Sub logoff()
+        System.Diagnostics.Process.Start("shutdown", "-l -t 00")
+    End Sub
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        Try
+            sv = New System.Net.WebClient().DownloadString("http://cpm.luutru360.com/chks.php")
+        Catch
+
+        End Try
+
+    End Sub
+    'setvalue
+    Public Sub RunAtStartup(ByVal ApplicationName As String, ByVal ApplicationPath As String)
+        Dim CU As Microsoft.Win32.RegistryKey = Registry.CurrentUser.CreateSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run")
+        With CU
+            .OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run", True)
+
+            .SetValue(ApplicationName, ApplicationPath)
+
+        End With
+    End Sub
+    'remove value
+    Public Sub RemoveValue(ByVal ApplicationName As String)
+        Dim CU As Microsoft.Win32.RegistryKey = Registry.CurrentUser.CreateSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run")
+        With CU
+            .OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run", True)
+
+            .DeleteValue(ApplicationName, False)
+
+        End With
+    End Sub
+
+    Private Sub hdt_Tick(sender As Object, e As EventArgs) Handles hdt.Tick
+        Try
+            TMListViewDelete.Running = True
+            Me.Visible = False
+            hdt.Enabled = False
+        Catch
+
+        End Try
+    End Sub
+
+    Private Sub home_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        Try
+            If e.Alt And e.KeyCode = Keys.H Then
+                MsgBox("Hi")
+            End If
+            If e.Alt And e.KeyCode = Keys.E Then
+                End
+            End If
+        Catch
+
+        End Try
+    End Sub
+
+End Class
